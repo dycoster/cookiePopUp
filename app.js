64096440284ac81c4c116b34e58811d36cb9f088
Your Name <61963464+dycoster@users.noreply.github.com>
2022-01-24 14:35:56 +1100

    animation complete

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-const tl= gsap.timeline({defaults: {duration: 0.75}})
+const tl= gsap.timeline({defaults: {duration: 0.75, ease: 'power2.out'}})
 
 tl.fromTo('.cookie-container', {scale: 0}, {scale: 1})
 tl.fromTo('.cookie', {opacity: 0, x:-50, rotation: '-45deg'}, {opacity: 1, x: 0, rotation: 0})
+tl.fromTo('.text', {opacity: 0, x: 30}, {opacity: 1, x:0}, '<')
+
+tl.fromTo('.cookie', {y:0, rotation: 0}, {y:-20, yoyo: true, repeat: -1, rotation: '-20deg'})
+tl.fromTo('#Crumbs', {y:0}, {y:-20, yoyo: true, repeat: -1}, '<')
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    gsap.to('.cookie-container', {opacity: 0, y: 100, duration: 0.75, ease: 'power2.out'})
+})
